949eb3ddfedde35e204f9f629a7938ceade09f20
rahimvaliyev <rahimvaliyev99@gmail.com>
2023-11-27 16:49:33 +0400

    fetch-chat-api

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,5 +1,9 @@
+import { useContext } from "react"
+import { ChatContext } from "../context/ChatContext"
 
 const Chat = () => {
+  const { userChats,isUserChatsLoading,userChatsError } = useContext(ChatContext)
+  console.log("UserChats",userChats);
   return (
     <div>Chat</div>
   )
--- client/src/utils/service.js
+++ client/src/utils/service.js
@@ -17,10 +17,25 @@ export const postRequest = async (url, body) => {
         } else {
             message = data
         }
-        return {error : true , message}
+        return { error: true, message }
     }
 
     return data
+}
 
+export const getRequest = async (url) => {
+    const response = await fetch(url)
+
+    const data = await response.json()
+    if (!response.ok) {
+        let message = "An error occured..."
+        if (data?.message) {
+            message = data.message
+        } else {
+            message = data
+        }
+        return { error: true, message }
+    }
+    return data
 
 }
