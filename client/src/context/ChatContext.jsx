5747b9d77d3fe2a4d4381421eefabea0be40d6fd
rahimvaliyev <rahimvaliyev99@gmail.com>
2023-11-28 00:24:31 +0400

    get-messages

--- client/src/context/ChatContext.jsx
+++ client/src/context/ChatContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useCallback, useEffect, useState } from "react";
 import { baseUrl , getRequest , postRequest } from "../utils/service";
 
 export const ChatContext = createContext()
@@ -7,31 +7,94 @@ export const ChatContextProvider = ({children,user})=>{
     const [userChats, setUserChats] = useState(null)
     const [isUserChatsLoading, setIsUserChatsLoading] = useState(false);
     const [userChatsError,setUserChatsError] = useState(null);
+    const [potentialChats , setPotentialChats] = useState([]);
+    const [currentChat ,setCurrentChat] = useState(null);
+    const [messages ,setMessages] = useState(null)
+    const [isMessagesLoading, setIsMessagesLoading] = useState(false)
+    const [messagesError,setMessagesError] = useState(null)
+
+
+    useEffect(()=>{
+
+      const getUsers = async ()=> {
+        const response = await getRequest(`${baseUrl}/users`);
+        if(response.error){
+          return console.log("Error fetching users",response);
+        }
+        const pCharts =  response.filter((u)=>{
+          let isChartCreated = false
+          if(user?._id === u._id) return false;
+
+          if(userChats){
+           isChartCreated = userChats?.some((chat)=>{
+              return chat.members[0] === u._id || chat.members[1] === u._id
+            })
+          }
+
+          return !isChartCreated 
+        })
+        setPotentialChats(pCharts)
+      }
+      getUsers()
+
+    },[userChats])
 
    useEffect(()=>{
     const getUserChats = async ()=>{
       if(user?._id){
         setIsUserChatsLoading(true)
         setUserChatsError(null)
-        const response = await getRequest(`${baseUrl}/chats/${user?._id}`)
 
+        const response = await getRequest(`${baseUrl}/chats/${user?._id}`)
+        setIsUserChatsLoading(false)
         if(response.error){
             return setUserChatsError(response)
         }
-
-        setIsUserChatsLoading(false)
-
         setUserChats(response)
       }
     }
     getUserChats()
    },[user])
 
+   useEffect(()=>{
+    const getMessages = async ()=>{
+        setIsMessagesLoading(true)
+        setMessagesError(null)
+
+        const response = await getRequest(`${baseUrl}/messages/${currentChat?._id}`)
+        setIsMessagesLoading(false)
+        if(response.error){
+            return setMessagesError(response)
+        }
+        setMessages(response)
+    }
+    getMessages()
+   },[currentChat])
+
+   const updateCurrentChat = useCallback((chat)=>{
+    setCurrentChat(chat)
+   },[])
+
+   const createChat = useCallback(async (firstId , secondId)=>{
+    const response = await postRequest(`${baseUrl}/chats`,JSON.stringify({firstId,secondId}))
+    if(response.error){
+      return console.log("Error creating chat",response);
+    }
+    setUserChats((prev)=>[...prev , response])
+   },[])
+
   return <ChatContext.Provider 
   value={{
     userChats,
     isUserChatsLoading,
-    userChatsError
+    userChatsError,
+    potentialChats,
+    createChat,
+    updateCurrentChat,
+    messages,
+    isMessagesLoading,
+    messagesError,
+    currentChat
   }}
   >
     {children}
--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -3,25 +3,30 @@ import { ChatContext } from "../context/ChatContext"
 import {AuthContext} from "../context/AuthContext"
 import {Container, Stack} from "react-bootstrap"
 import UserChat from "../components/chats/UserChat"
+import PotentialChats from "../components/chats/PotentialChats"
+import ChatBox from "../components/chats/ChatBox"
 
 const Chat = () => {
   const {user} = useContext(AuthContext)
-  const { userChats,isUserChatsLoading,userChatsError } = useContext(ChatContext)
+  const { userChats,isUserChatsLoading,updateCurrentChat } = useContext(ChatContext)
   return (
     <>
     <Container>
+      <PotentialChats />
+
       {
         userChats?.length < 1 ? null : (
           <Stack direction="horizontal" gap={4} className="align-items-start">
             <Stack className="messages-box flex-grow-0 pe-3" >
               {isUserChatsLoading && <p>Loading chats...</p>}
+
               {userChats?.map((chat,index)=>(
-                <div key={index}>
-                    <UserChat chat={chat} user ={user && user} />
+                <div key={index} onClick={()=> updateCurrentChat(chat)}  >
+                    <UserChat chat={chat} user ={user} />
                 </div>
               ))}
             </Stack>
-            <p>ChatBox</p>
+            <ChatBox />
           </Stack>
         )
       }
